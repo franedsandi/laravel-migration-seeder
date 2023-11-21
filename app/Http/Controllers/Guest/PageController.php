4ba15cbef06334e437c1a84bec53be28333e6272
franedsandi <franedsandi@gmail.com>
2023-11-21 19:12:22 +0100

    [feat] model train and train in db

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
-    public function index(){
-        return view('home');
+    public function trains(){
+        $trains = Train::all();
+        dump($trains);
+        return view('home', compact('trains'));
     }
 }
